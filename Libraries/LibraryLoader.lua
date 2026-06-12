3ae685d5610731d989686a56b6857f9813fcc5ac
StarRose926 <businessburgerparty@gmail.com>
2026-06-13 00:08:23 +0200

    Update LibraryLoader.lua

--- Libraries/LibraryLoader.lua
+++ Libraries/LibraryLoader.lua
@@ -1,8 +1,9 @@
+local path = '=mac-fries/Libraries/%s'
 local url = 'https://raw.githubusercontent.com/StarRose926/MacFriesHub/refs/heads/main/Libraries/%s.lua'
 
 local libraries = {}
 local function load(name)
-    libraries[name] = loadstring(game:HttpGet(string.format(url, name)))
+    libraries[name] = loadstring(game:HttpGet(string.format(url, name)), string.format(path, name))()
 end
 
 load('Bit')
