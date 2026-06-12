145152a977ac2832168bb4b3a185a84287f50500
StarRose926 <businessburgerparty@gmail.com>
2026-06-12 22:37:57 +0200

    Update isnetworkowner.lua

--- Libraries/isnetworkowner.lua
+++ Libraries/isnetworkowner.lua
@@ -1,6 +1,8 @@
 -- BY: StarRose
 --
 -- Fallback for executors like "Solara" again :skull:
+--
+-- NOTE: Patched :( - does not work anymore
 
 return function(part)
 	return part.Anchored == false and part.ReceiveAge == 0
