015303642b7e04b6f65b551a474cbbb9744a1d42
StarRose926 <businessburgerparty@gmail.com>
2026-06-15 20:43:56 +0200

    Update getsenv.lua

--- ExtraLibraries/Tests/tests/getsenv.lua
+++ ExtraLibraries/Tests/tests/getsenv.lua
@@ -3,6 +3,8 @@ return function(test)
     local senv = getsenv(Script)
 
     test.assert(senv, `Could not retrieve the script-env from the Script ({Script:GetFullName()})`)
+    if not senv then return end
+    
     test.assert(senv.script == Script, `"script" is not set to the correct Script ({Script:GetFullName()})`)
     test.assert(senv._G ~= _G, '"_G" should not be shared!')
 end
