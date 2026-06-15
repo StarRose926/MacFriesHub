821b2984a7a06367b3fc6dccedb30d15ba4342e8
StarRose926 <businessburgerparty@gmail.com>
2026-06-15 20:38:39 +0200

    Update getsenv.lua

--- ExtraLibraries/Tests/tests/getsenv.lua
+++ ExtraLibraries/Tests/tests/getsenv.lua
@@ -2,6 +2,7 @@ return function(test)
     local Script = game:FindFirstChildWhichIsA('LocalScript', true)
     local senv = getsenv(Script)
 
+    test.assert(senv, `Could not retrieve the script-env from the Script ({Script:GetFullName()})`)
     test.assert(senv.script == Script, `"script" is not set to the correct Script ({Script:GetFullName()})`)
     test.assert(senv._G ~= _G, '"_G" should not be shared!')
 end
