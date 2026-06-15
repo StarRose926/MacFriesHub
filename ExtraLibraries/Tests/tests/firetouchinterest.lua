6937abb3bbe6f75532e7c8a12b6ab24ce25c5cd0
StarRose926 <businessburgerparty@gmail.com>
2026-06-15 19:33:11 +0200

    Create firetouchinterest.lua

---
+++ ExtraLibraries/Tests/tests/firetouchinterest.lua
@@ -0,0 +1,23 @@
+return function(test)
+    local rootPart = game:GetService('Players').LocalPlayer.Character.PrimaryPart
+  
+    local part, part_2 = Instance.new('Part', workspace), Instance.new('Part', workspace)
+    test.clean(part)
+    part.Transparency = 1
+    part.CanCollide = false
+    part.Anchored = true
+    part.Position = rootPart.Position + Vector3.new(100, 100, 100)
+
+    local did_fire = false
+    part.Touched:Connect(function(touched)
+        if touched == rootPart then
+            did_fire = true
+        end
+    end)
+
+    firetouchinterest(part, rootPart, 1)
+
+    task.wait(0.5)
+
+    test.assert(did_fire, 'Did not touch the Part with the Client(s) Character')
+end
