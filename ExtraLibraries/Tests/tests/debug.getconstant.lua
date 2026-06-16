b0cfe6f690a36935d2024751501f08f163d9903e
StarRose926 <businessburgerparty@gmail.com>
2026-06-16 17:21:32 +0200

    Create debug.getconstant.lua

---
+++ ExtraLibraries/Tests/tests/debug.getconstant.lua
@@ -0,0 +1,9 @@
+local function(test)
+    local function closure()
+        print("Constants!")
+    end
+
+    local r1, r2 = debug.constants(closure, 1) == 'print', debug.constant(closure, 2) == 'Constants!'
+    test.assert(r1, 'Did not return the correct constant for (constant 1) in the closure provided!')
+    test.assert(r2, 'Did not return the correct constant for (constant 2) in the closure provided!')
+end
