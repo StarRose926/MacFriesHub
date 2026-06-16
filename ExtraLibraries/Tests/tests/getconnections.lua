82b9069c3bf8a247cf46aac3e49356329f69a0b0
StarRose926 <businessburgerparty@gmail.com>
2026-06-16 15:58:03 +0200

    Update getconnections.lua

--- ExtraLibraries/Tests/tests/getconnections.lua
+++ ExtraLibraries/Tests/tests/getconnections.lua
@@ -1,6 +1,8 @@
 return function(test)
     local bindable = Instance.new('BindableEvent')
+    local waitableEvent = Instance.new('BindableEvent')
     test.clean(bindable)
+    test.clean(waitableEvent)
 
     local amount = math.random(1, 3)
 
@@ -10,6 +12,7 @@ return function(test)
         test.clean(bindable.Event:Connect(function(res)
             if res == i then
                 fired_by[i] = true
+                waitableEvent:Fire()
             end
         end))
     end
@@ -21,13 +24,22 @@ return function(test)
 
     for i, con in connections do
         test.assert(type(con.Function) == 'function', `Function for index {i} did not return a Function! (got {type(con.Function)})`)
-        con:Fire(i)
-
-        -- Roblox Queue maybe delayed!
-        --
-        -- we will do 1 second, just to be safe, because of Roblox being super annoying here!
-        task.wait(1)
-
-        test.assert(fired_by[i], `Did not fire the Connection with Argument ({i})`)
     end
+
+    local con = connections[1]
+
+    local running = coroutine.running()
+    local thread = task.delay(function()
+        test.assert(fired_by[1], 'Did fire the Connection (1)')
+        task.spawn(running)
+    end, 5)
+
+    task.spawn(function()
+        waitableEvent.Event:Wait()
+        task.cancel(thread)
+    end)
+
+    con:Fire(1)
+
+    return coroutine.yield()
 end
