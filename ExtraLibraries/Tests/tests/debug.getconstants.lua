7796e0b178d67df44fa45f712575d30c79ee74f9
StarRose926 <businessburgerparty@gmail.com>
2026-06-16 17:17:50 +0200

    Create debug.getconstants.lua

---
+++ ExtraLibraries/Tests/tests/debug.getconstants.lua
@@ -0,0 +1,25 @@
+local function compare(t1, t2)
+    for i, v in t1 do
+        if t2[i] ~= v then
+            return false
+        end
+    end
+
+    return true
+end
+
+return function(test)
+    local function closure()
+        print("Hello world")
+    end
+
+    local constants = debug.getconstants(closure)
+
+    test.assert(type(constants) == 'table', `Constants result expected to be a Table (got {type(constants)})`)
+    if type(constants) ~= 'table' then return end
+
+    test.assert(compare(constants, {
+        "print",
+        "Hello world"
+    }))
+end
