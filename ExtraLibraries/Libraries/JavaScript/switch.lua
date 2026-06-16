1cfbfa96c72e3f60d69d0bec4a7ab07c2030921c
StarRose926 <businessburgerparty@gmail.com>
2026-06-16 20:01:31 +0200

    Create switch.lua

---
+++ ExtraLibraries/Libraries/JavaScript/switch.lua
@@ -0,0 +1,25 @@
+local function switch(expression)
+    local s = {
+        _found_match = false
+    }
+
+    function s:case(obj, onMatch)
+        if obj == expression and not self._on_found_match then
+            self._found_match = true
+            onMatch()
+        end
+
+        return s
+    end
+
+    function s:default(on)
+        if not self._found_match then
+            self._on_found_match = true
+            on()
+        end
+    end
+
+    return s
+end
+
+return switch
