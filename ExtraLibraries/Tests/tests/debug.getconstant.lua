087f3b1f895af13fcb22e69ef21c4ab5ea5f2884
StarRose926 <businessburgerparty@gmail.com>
2026-06-16 17:23:15 +0200

    Update debug.getconstant.lua
    spelling error on my end SORRY!

--- ExtraLibraries/Tests/tests/debug.getconstant.lua
+++ ExtraLibraries/Tests/tests/debug.getconstant.lua
@@ -3,7 +3,7 @@ local function(test)
         print("Constants!")
     end
 
-    local r1, r2 = debug.constants(closure, 1) == 'print', debug.constant(closure, 2) == 'Constants!'
+    local r1, r2 = debug.getconstant(closure, 1) == 'print', debug.getconstant(closure, 2) == 'Constants!'
     test.assert(r1, 'Did not return the correct constant for (constant 1) in the closure provided!')
     test.assert(r2, 'Did not return the correct constant for (constant 2) in the closure provided!')
 end
