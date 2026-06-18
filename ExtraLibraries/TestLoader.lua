a0bbf11b2737d7830bb026c1852bbd140a8c4fdd
StarRose926 <businessburgerparty@gmail.com>
2026-06-18 17:56:37 +0200

    Update TestLoader.lua

--- ExtraLibraries/TestLoader.lua
+++ ExtraLibraries/TestLoader.lua
@@ -10,18 +10,17 @@ local test_info = HttpService:JSONDecode(game:HttpGet('https://raw.githubusercon
 local url = 'https://raw.githubusercontent.com/StarRose926/MacFriesHub/refs/heads/main/ExtraLibraries/Tests/tests/%s.lua'
 
 for _, v in test_info.tests do
-    if typeof(v) == 'string' then
-        local test_str = game:HttpGet(string.format(url, v))
+    local name = type(v) == 'table' and v[1] or v
+    local test_str = game:HttpGet(string.format(url, name))
 
-        if test_str ~= '404: Not Found' then
-            local func = loadstring(test_str)()
+    if test_str ~= '404: Not Found' then
+        local func = loadstring(test_str)()
             
-            local data = game:HttpGet(('https://raw.githubusercontent.com/StarRose926/MacFriesHub/refs/heads/main/Executor/%s.luau'):format(v))
+        local data = game:HttpGet(('https://raw.githubusercontent.com/StarRose926/MacFriesHub/refs/heads/main/Executor/%s.luau'):format(v))
 
-            TestLib.create(v, func, data ~= '404: Not Found' and function()
-                return loadstring(data)()
-            end)
-        end
+        TestLib.create(v, func, data ~= '404: Not Found' and function()
+            return loadstring(data)()
+        end)
     end
 end
 
