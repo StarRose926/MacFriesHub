ec351e68b77a4ed27f8404534082e79640081531
StarRose926 <businessburgerparty@gmail.com>
2026-06-18 17:22:25 +0200

    Create ishooked.lua

---
+++ ExtraLibraries/Tests/tests/ishooked.lua
@@ -0,0 +1,17 @@
+return function(test)
+    local did_hookfunction_succeed = test.did_succeed('hookfunction')
+  
+    test.assert(did_hookfunction_succeed, 'Cannot run this test medout hookfunction being successfull!')
+    if not did_hookfunction_succeed then return end
+  
+    local function closure()
+        return 'Good!'
+    end
+
+    test.assert(ishooked(closure) == false, 'Did not return false when the closure was not hooked')
+    
+    hookfunction(closure, function()
+        return 'HOOKED'
+    end)
+    test.assert(ishooked(closure) == true, 'Did not return true when the closure was hooked')
+end
