465dcf14d5e6c3eb67cea126d392e513cc82ad4d
StarRose926 <businessburgerparty@gmail.com>
2026-07-01 12:46:45 +0200

    Update loader.lua

--- Libraries/External/ExternalLoader/loader.lua
+++ Libraries/External/ExternalLoader/loader.lua
@@ -138,8 +138,6 @@ local function collapseInitModules(root, OutputFolder)
 	end
 end
 
-print("Extracting packages into Roblox layout...")
-
 local loader = {}
 
 loader.put = function(loaded_package, name, parent)
@@ -183,11 +181,6 @@ loader.load = function(git, zip_info, inst_info, fixes)
 	
 	-- 3. Optimized extraction loop (Kept completely untouched)
 	for _, name, offset, size, packed, crc in zzlib.files(zipContent) do
-		if string.find(name, "clz32") then
-			print("FOUND CLZ32 IN ZIP:", name)
-		end
-		
-		print(name, offset, size, packed)
 		if not string.match(name, "/$") and string.find(name, string.format("%s/%s/", zip_info.main_zip, zip_info.src_folder), 1, true) then
 
 			-- Clean up path (removes the GitHub root folder)
@@ -239,8 +232,6 @@ loader.load = function(git, zip_info, inst_info, fixes)
 						module.Parent = targetFolder
 
 						cache:set(module, fileData)
-						
-						print('Fixed file corruption:', name)
 					else
 						warn("Failed to extract:", name, ' -', fileData)
 					end
