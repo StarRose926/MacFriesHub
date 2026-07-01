38c817f9c9301cbed5aaee0b333aa5713c6639e1
StarRose926 <businessburgerparty@gmail.com>
2026-07-01 17:39:07 +0200

    Update ScriptFakeLoader.lua

--- Libraries/ScriptFakeLoader.lua
+++ Libraries/ScriptFakeLoader.lua
@@ -1,96 +1,120 @@
 local cache = {}
+local script_env_changes = {}
 
 local clonefunc = clonefunc or clonefunction or function(func)
-    return function(...)
-        return func(...)
-    end
+	return function(...)
+		return func(...)
+	end
+end
+
+local function setScriptEnvirementGlobal(script, glob, newGlob)
+	script_env_changes[script] = script_env_changes[script] or {}
+	
+	script_env_changes[script][glob] = newGlob
 end
 
 local function makeCacheRequire(module)
-    local moduleCache = cache[module]
+	local moduleCache = cache[module]
 
-    if moduleCache then
-        if not moduleCache.cache then
-            moduleCache.cache = moduleCache.Function()
-        end
+	if moduleCache then
+		if not moduleCache.cache then
+			moduleCache.cache = moduleCache.Function()
+		end
 
-        return moduleCache.cache
-    end
+		return moduleCache.cache
+	end
 
-    return require(module)
+	return require(module)
 end
 
 local function makeModuleCache(module, code, path)
-    local fenv = getfenv()
-    local req = clonefunc(makeCacheRequire)
+	local fenv = getfenv()
+	local req = clonefunc(makeCacheRequire)
+	
+	local spesific_env = script_env_changes[module]
 
-    local env = setmetatable({}, {
-        __index = function(self, index)
-            if index == 'require' then
-                return req
-            elseif index == 'script' then
-                return module
-            end
+	local env = setmetatable({}, {
+		__index = function(self, index)
+			if index == 'require' then
+				return req
+			elseif index == 'script' then
+				return module
+			elseif spesific_env and spesific_env[index] then
+				return spesific_env[index]
+			end
 
-            return fenv[index]
-        end,
-        __newindex = function(self, index, newIndex)
-            if index == 'require' then
-                req = newIndex
-                return
-            elseif index == 'script' then
-                module = newIndex
-                return
-            end
+			return fenv[index]
+		end,
+		__newindex = function(self, index, newIndex)
+			if index == 'require' then
+				req = newIndex
+				return
+			elseif index == 'script' then
+				module = newIndex
+				return
+			elseif spesific_env and spesific_env[index] then
+				spesific_env[index] = newIndex
+				fenv[index] = newIndex
+				return
+			end
 
-            fenv[index] = newIndex
-        end
-    })
+			fenv[index] = newIndex
+		end
+	})
 
-    local func = loadstring(code, '=' .. (path or module:GetFullName()))
-    setfenv(func, env)
+	local func = loadstring(code, '=' .. (path or module:GetFullName()))
+	setfenv(func, env)
 
-    cache[module] = {
-        cache = nil,
-        Function = func
-    }
+	cache[module] = {
+		cache = nil,
+		Function = func
+	}
 end
 
 local function makeLocalScript(script, code, path)
-    local fenv = getfenv()
-    local req = clonefunc(makeCacheRequire)
+	local fenv = getfenv()
+	local req = clonefunc(makeCacheRequire)
+	
+	local spesific_env = script_env_changes[module]
 
-    local env = setmetatable({}, {
-        __index = function(self, index)
-            if index == 'require' then
-                return req
-            elseif index == 'script' then
-                return script
-            end
+	local env = setmetatable({}, {
+		__index = function(self, index)
+			if index == 'require' then
+				return req
+			elseif index == 'script' then
+				return script
+			elseif spesific_env and spesific_env[index] then
+				return spesific_env[index]
+			end
 
-            return fenv[index]
-        end,
-        __newindex = function(self, index, newIndex)
-            if index == 'require' then
-                req = newIndex
-                return
-            elseif index == 'script' then
-                script = newIndex
-                return
-            end
+			return fenv[index]
+		end,
+		__newindex = function(self, index, newIndex)
+			if index == 'require' then
+				req = newIndex
+				return
+			elseif index == 'script' then
+				script = newIndex
+				return
+			elseif spesific_env and spesific_env[index] then
+				spesific_env[index] = newIndex
+				fenv[index] = newIndex
+				return
+			end
 
-            fenv[index] = newIndex
-        end
-    })
+			fenv[index] = newIndex
+		end
+	})
 
-    local func = loadstring(code, '=' .. (path or script:GetFullName()))
-    setfenv(func, env)
+	local func = loadstring(code, '=' .. (path or script:GetFullName()))
+	setfenv(func, env)
 
-    return func()
+	return func()
 end
 
 return {
-    makeCacheRequire = makeCacheRequire,
-    makeModuleCache = makeModuleCache,
-    makeLocalScript = makeLocalScript
+	setScriptEnvirementGlobal = setScriptEnvirementGlobal,
+	makeCacheRequire = makeCacheRequire,
+	makeModuleCache = makeModuleCache,
+	makeLocalScript = makeLocalScript
 }
