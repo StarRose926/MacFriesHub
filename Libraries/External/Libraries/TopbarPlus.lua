ac6e899632e16bcdec818089e5db675c3ea9bb26
StarRose926 <businessburgerparty@gmail.com>
2026-07-01 17:59:49 +0200

    Update TopbarPlus.lua

--- Libraries/External/Libraries/TopbarPlus.lua
+++ Libraries/External/Libraries/TopbarPlus.lua
@@ -1,6 +1,11 @@
+-- Main code from (1ForeverHD): https://github.com/1ForeverHD/TopbarPlus/tree/main
+-- TopbarPlus LICENSE: https://github.com/1ForeverHD/TopbarPlus/tree/main?tab=License-1-ov-file
+--
+-- NOTE: LOADER CODE NOT BY 1ForeverHD
+
 local loader = loadstring(game:HttpGet('https://raw.githubusercontent.com/StarRose926/MacFriesHub/refs/heads/main/Libraries/External/ExternalLoader/loader.lua'))()
 
-return function(Packages)
+return function(Packages, ScriptFakeLoader)
     local TopbarPlusModule = loader.load({
         user = '1ForeverHD',
         repo = 'TopbarPlus',
@@ -18,5 +23,13 @@ return function(Packages)
     TopbarModule.Parent = Packages
     TopbarPlusModule:Destroy()
 
+    -- Set and reload env!
+    ScriptFakeLoader.setScriptEnvirementGlobal(TopbarModule.Attribute, 'print', function(...) end)
+    ScriptFakeLoader.makeModuleCache(TopbarModule.Attribute, game:HttpGet('https://raw.githubusercontent.com/1ForeverHD/TopbarPlus/refs/heads/main/src/Attribute.lua'))
+
+    -- Lets fix it up a little, shall we?
+    ScriptFakeLoader.makeModuleCache(TopbarModule.Reference, game:HttpGet('https://raw.githubusercontent.com/1ForeverHD/TopbarPlus/refs/heads/main/src/Reference.lua'):gsub('ReplicatedStorage', 'RobloxReplicatedStorage'))
+    ScriptFakeLoader.makeModuleCache(TopbarModule, game:HttpGet('https://raw.githubusercontent.com/1ForeverHD/TopbarPlus/refs/heads/main/src/init.lua'):gsub('localPlayer:WaitForChild(%"PlayerGui%")', 'game:GetService(%"CoreGui%")'))
+    
     return TopbarModule
 end
