6f5e56063cb332214d5dc5829eac23cd1a5a2f59
StarRose926 <businessburgerparty@gmail.com>
2026-07-08 15:02:48 +0200

    Delete ExtraLibraries directory

--- ExtraLibraries/Tests/tests/debug.getconstant.lua
+++
@@ -1,9 +0,0 @@
-return function(test)
-    local function closure()
-        print("Constants!")
-    end
-
-    local r1, r2 = debug.getconstant(closure, 1) == 'print', debug.getconstant(closure, 2) == 'Constants!'
-    test.assert(r1, 'Did not return the correct constant for (constant 1) in the closure provided!')
-    test.assert(r2, 'Did not return the correct constant for (constant 2) in the closure provided!')
-end
