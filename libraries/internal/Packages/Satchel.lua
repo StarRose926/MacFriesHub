8d5dc59f7767dfabf4661bba3a049e98e4d68f3d
StarRose926 <businessburgerparty@gmail.com>
2026-07-12 01:31:14 +0200

    Update Satchel.lua
    Fix "TopbarPlus" misspelling

--- libraries/internal/Packages/Satchel.lua
+++ libraries/internal/Packages/Satchel.lua
@@ -1,6 +1,6 @@
 return function(_, ScriptService)
     local Source = game:HttpGet('https://raw.githubusercontent.com/ryanlua/satchel/refs/heads/main/src/init.luau')
-    local MainSource = Source:gsub('BindAction', 'BindCoreAction'):gsub('UnbindAction', 'UnbindCoreAction'):gsub('require%(script.Attribution%)', '')
+    local MainSource = Source:gsub('BindAction', 'BindCoreAction'):gsub('UnbindAction', 'UnbindCoreAction'):gsub('require%(script.Attribution%)', ''):gsub('require%(script.Parent.topbarplus%)', 'require(script.Parent.TopbarPlus)')
 
     local Module = Instance.new('ModuleScript')
     Module.Name = 'Stachel'
