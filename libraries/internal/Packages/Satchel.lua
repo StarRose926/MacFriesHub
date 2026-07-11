145371e140d8f28357361c439e1806ea57e6bfd1
StarRose926 <businessburgerparty@gmail.com>
2026-07-12 01:49:29 +0200

    Update Satchel.lua

--- libraries/internal/Packages/Satchel.lua
+++ libraries/internal/Packages/Satchel.lua
@@ -1,9 +1,12 @@
+local cloneref = cloneref or function(obj) return obj end
+
 return function(_, ScriptService)
     local Source = game:HttpGet('https://raw.githubusercontent.com/ryanlua/satchel/refs/heads/main/src/init.luau')
-    local MainSource = Source:gsub('BindAction', 'BindCoreAction'):gsub('UnbindAction', 'UnbindCoreAction'):gsub('require%(script.Attribution%)', ''):gsub('require%(script.Parent.topbarplus%)', 'require(script.Parent.TopbarPlus)')
+    local MainSource = Source:gsub('BindAction', 'BindCoreAction'):gsub('UnbindAction', 'UnbindCoreAction'):gsub('require%(script.Attribution%)', ''):gsub('require%(script.Parent.topbarplus%)', 'require(script.Parent.TopbarPlus)'):gsub('Players.LocalPlayer:WaitForChild%("PlayerGui"%)', 'game:GetService("CoreGui").MacFries.Guis')
 
     local Module = Instance.new('ModuleScript')
     Module.Name = 'Stachel'
+    Module.Parent = cloneref(game:GetService('CoreGui')).MacFries.Modules
 
     ScriptService.makeModuleCache(Module, MainSource)
 
