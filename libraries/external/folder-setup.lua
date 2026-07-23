43272def4226ef8a3d381e9f38793bd04d3ef16b
StarRose926 <businessburgerparty@gmail.com>
2026-07-23 14:10:13 +0200

    Create folder-setup.lua

---
+++ libraries/external/folder-setup.lua
@@ -0,0 +1,24 @@
+local create_placeid = ...
+
+local function _makefolder(path)
+    if not isfolder(path) then
+        makefolder(path)
+    end
+end
+
+local function _writefile(path, content)
+    if not isfile(path) then
+        writefile(path, content)
+    end
+end
+
+_makefolder('MacFries')
+_makefolder('MacFries/Saves')
+
+if create_placeid then
+    _makefolder('MacFries/Saves/' .. tostring(game.PlaceId))
+end
+
+_writefile('MacFries/Registry', '')
+
+return 'MacFries'
